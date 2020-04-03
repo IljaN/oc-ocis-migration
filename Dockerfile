32178383693641122aeaad65b4d70e71609743f5
Ilja Neumann <node512@gmail.com>
2020-04-03 15:18:30 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM owncloud/server:latest
+ADD openidconnect-0.2.0.tar.gz /var/www/owncloud/apps/
---
+++ openidconnect.config.php
@@ -0,0 +1,12 @@
+<?php
+$CONFIG = array(
+'openid-connect' =>
+  array (
+    'provider-url' => 'https://localhost:9200',
+    'client-id' => 'phoenix',
+    'client-secret' => 'super',
+    'loginButtonName' => 'OpenId Connect',
+    'search-attribute' => 'preferred_username',
+    'mode' => 'userid',
+  )
+);
